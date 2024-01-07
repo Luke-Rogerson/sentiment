a45384529f03bce4e74eed0284ee14c4219e672e
Luke Rogerson <contact@lukerogerson.me>
2024-01-07 22:23:42 +0000

    Add model loading status

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@
  */
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { Thumbs } from "./components/thumbs";
 
-import { pipeline, env } from "@xenova/transformers";
+import { env, pipeline } from "@xenova/transformers";
+import { LoaderIcon } from "./components/loader";
 import { Sentiment } from "./types";
 
 // Skip local model check
@@ -25,10 +25,10 @@ type Result = {
 };
 
 export default function App() {
+  const [modelStatus, setModelStatus] = useState("");
   const [statement, setStatement] = useState("I love this app!");
   const classifier = useRef<TextClassificationPipeline | null>(null);
   const [result, setResult] = useState<Result | null>(null);
-  console.log("result :", result);
   const [loading, setLoading] = useState(true);
 
   const queryModel = useCallback((query: string) => {
@@ -41,15 +41,23 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
-      classifier.current = await pipeline(TASK, MODEL);
+      classifier.current = (await pipeline(TASK, MODEL, {
+        progress_callback: progressUpdate,
+      })) as unknown as TextClassificationPipeline;
       queryModel(statement);
     })();
-  }, []);
+  }, [queryModel, statement]);
 
   useEffect(() => {
     setLoading(true);
     queryModel(statement);
-  }, [statement]);
+  }, [queryModel, statement]);
+
+  const progressUpdate = ({ status }: { status: string }) => {
+    if (status === "ready") {
+      setModelStatus(status);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 p-4">
@@ -64,17 +72,24 @@ export default function App() {
         <div className="grid gap-6">
           <div className="grid w-full gap-1.5">
             <Label htmlFor="message">Your Message</Label>
-            <Textarea
-              id="message"
-              placeholder="Type your message here..."
-              value={statement}
-              onChange={(e) => setStatement(e.target.value)}
-              disabled={loading}
-            />
+
+            <div className="relative">
+              {" "}
+              <Textarea
+                id="message"
+                placeholder="Type your message here..."
+                value={statement}
+                onChange={(e) => setStatement(e.target.value)}
+                disabled={loading}
+              />
+              {modelStatus !== "ready" && (
+                <div className="absolute inset-0 flex items-center justify-center bg-yellow-500 text-white px-4 py-2 rounded-md -rotate-12">
+                  <LoaderIcon className="h-4 w-4 animate-spin" />
+                  <span>Model (~1GB) loading...</span>
+                </div>
+              )}
+            </div>
           </div>
-          <Button className="w-full" disabled={loading}>
-            Analyze
-          </Button>
         </div>
 
         {!loading && result && statement.length > 0 && (
@@ -98,7 +113,7 @@ export default function App() {
           Built by{" "}
           <a
             className="text-blue-500 hover:underline"
-            href="https://www.github.com/Luke-Rogerson"
+            href="https://www.linkedin.com/in/lukerogerson/"
             target="_blank"
           >
             Luke Rogerson
@@ -106,7 +121,7 @@ export default function App() {
           . Check out{" "}
           <a
             className="text-blue-500 hover:underline"
-            href="https://www.github.com/Luke-Rogerson"
+            href="https://github.com/Luke-Rogerson/sentiment"
             target="_blank"
           >
             the source code
